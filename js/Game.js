044cb6d60e59d1ad7958a2026f3979952ec3eb24
Atharva <archisdhuri@gmail.com>
2021-06-18 17:39:34 +0530

    Multiplayer Car Racing Game Stage 5

--- js/Game.js
+++ js/Game.js
@@ -30,14 +30,20 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage('car1', carImg1);
     car2 = createSprite(300,200);
+    car2.addImage('car2', carImg2);
     car3 = createSprite(500,200);
+    car3.addImage('car3', carImg3);
     car4 = createSprite(700,200);
+    car4.addImage('car4', carImg4);
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
+    background(groundImg)
+    image(trackImg1, 0, -displayHeight*4, displayWidth, displayHeight*5);
 
     Player.getPlayerInfo();
     
@@ -48,7 +54,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +85,15 @@ class Game {
       player.update();
     }
 
+    if(player.distance >3790) {
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+
+  end() {
+    console.log("The Game Has Ended");
+    
   }
 }
